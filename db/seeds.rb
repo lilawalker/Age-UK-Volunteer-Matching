1e31724a6ff2b2d037314aab20306df62656ba31
ratcliffetj <ratcliffetj89@gmail.com>
2020-06-02 14:03:22 +0100

    added seeding of interests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# users = User.create([{ email: 'test@gmail.com', password: 'potato' }])
+users = User.create([{ id: 1000, email: 'test@gmail.com', password: 'potato', volunteer: true, name: 'Greg', telephone: "01235823674", location: 'Highcliff', bio: 'I really enjoy long walks in the park and baking on Sundays.' },
+                    { id: 1001, email: 'mark@example.com', password: 'markmark', volunteer: false, name: 'Mark', telephone: "01865478293", location: 'Wales', bio: 'I like to look over the hills and smell the fresh sea breeze.' }])
 
-users = User.create([{ email: 'test@gmail.com', password: 'potato', volunteer: true, name: 'Greg', telephone: "01235823674", location: 'Highcliff', bio: 'I really enjoy long walks in the park and baking on Sundays.' },
-                    { email: 'mark@example.com', password: 'markmark', volunteer: false, name: 'Mark', telephone: "01865478293", location: 'Wales', bio: 'I like to look over the hills and smell the fresh sea breeze.' }])
+Interest.new(name: 'sports').save(validate: false)
+Interest.new(name: 'reading').save(validate: false)
+Interest.new(name: 'baking').save(validate: false)
+Interest.new(name: 'gardening').save(validate: false)
 
+
+# Interest.create!(id: 2000, name: 'sports')
 # interests = Interest.create([{ name: 'sports' }, { name: 'baking' }, { name: 'reading' }, { name: 'walking' }, { name: 'story telling' }, { name: 'gardening' }, { name: 'crocheting' }])
 
 # interests_users = Interest_User.create([{ type: 'sports' }, { type: 'baking' }, { type: 'reading' }, { type: 'walking' }, { type: 'story telling' }, { type: 'gardening' }, { type: 'crocheting' }])
