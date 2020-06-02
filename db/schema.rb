87c8847ca7a611171e78e0cc954a65c3e4be6260
DavidStewartLDN <davidstewart.work@gmail.com>
2020-06-02 11:33:31 +0100

    Added migration for interests table
    updated seeds from type to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_163900) do
+ActiveRecord::Schema.define(version: 2020_06_02_102626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "interests", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([{ email: 'test@gmail.com', password: 'potato' }])
+# users = User.create([{ email: 'test@gmail.com', password: 'potato' }])
 
 users = User.create([{ email: 'test@gmail.com', password: 'potato', volunteer: true, name: 'Greg', telephone: "01235823674", location: 'Highcliff', bio: 'I really enjoy long walks in the park and baking on Sundays.' },
                     { email: 'mark@example.com', password: 'markmark', volunteer: false, name: 'Mark', telephone: "01865478293", location: 'Wales', bio: 'I like to look over the hills and smell the fresh sea breeze.' }])
 
-interests = Interest.create([{ type: 'sports' }, { type: 'baking' }, { type: 'reading' }, { type: 'walking' }, { type: 'story telling' }, { type: 'gardening' }, { type: 'crocheting' }])
+# interests = Interest.create([{ name: 'sports' }, { name: 'baking' }, { name: 'reading' }, { name: 'walking' }, { name: 'story telling' }, { name: 'gardening' }, { name: 'crocheting' }])
 
-interests_users = Interest_User.create([{ type: 'sports' }, { type: 'baking' }, { type: 'reading' }, { type: 'walking' }, { type: 'story telling' }, { type: 'gardening' }, { type: 'crocheting' }])
+# interests_users = Interest_User.create([{ type: 'sports' }, { type: 'baking' }, { type: 'reading' }, { type: 'walking' }, { type: 'story telling' }, { type: 'gardening' }, { type: 'crocheting' }])
+
+puts "Seeds have been set and you're ready to go!"
